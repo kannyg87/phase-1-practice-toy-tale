1f595263c6e9eb4f2e6e1375ab57f1528c401273
Kanny <kanny.ghafour87@gmail.com>
2023-07-30 09:23:28 -0500

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,79 @@
 let addToy = false;
+fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(el => {
+      console.log(el)
+      const div = document.querySelector('#toy-collection')
+      div.innerHTML += `<div class="card">
+        <h2>${el.name}</h2>
+        <img src="${el.image}" class="toy-avatar" />
+        <p>${el.likes}</p>
+        <button class="like-btn" id="${el.id}">Like ❤️</button>
+      </div> `;
+
+      const likeButton = document.querySelector(`[id="${el.id}"]`);
+      likeButton.addEventListener('click', () => {
+        increaseLikes(el);
+      });
+    });
+  });
+;
+
+const toyForm = document.querySelector('.add-toy-form');
+
+toyForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(toyForm);
+  const name = formData.get('name');
+  const image = formData.get('image');
+
+  const newToy = {
+    name,
+    image,
+    likes: 0,
+  };
+
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(newToy),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const card = createToyCard(data);
+      const toyCollection = document.getElementById('toy-collection');
+      toyCollection.appendChild(card);
+      toyForm.reset();
+    });
+});
+
+
+function increaseLikes(toy) {
+  const updatedToy = {
+    likes: toy.likes + 1,
+  };
+
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(updatedToy),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const toyLikes = document.querySelector(`[id="${toy.id}"]`).previousElementSibling;
+      toyLikes.textContent = `${data.likes} Likes`;
+    });
+}
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
